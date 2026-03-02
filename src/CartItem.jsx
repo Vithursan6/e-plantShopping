39b56ddad7c5271c0bb42fbf7b3f15e6ef111c81
vithursan6 <vithursanm@gmail.com>
2026-03-01 22:47:15 -0500

    fixed bugs

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,36 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+        totalCost += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    if(item.quantity > 0) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}));
+    } else {
+        dispatch(removeItem({ name: item.name }));
+    };
+    
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}))
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -274,8 +275,7 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                        
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
