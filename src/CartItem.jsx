8ee6125d0942cd7b6efad4091734092c2852a4af
vithursan6 <vithursanm@gmail.com>
2026-03-06 21:52:39 -0500

    added total count for each item and cart icon counter

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -47,7 +47,14 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+       totalCost += parseFloat(item.cost.substring(1)) * item.quantity;
+    
+        return totalCost;
   };
+
+    
+
 
   return (
     <div className="cart-container">
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -291,14 +291,13 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = plant => {
-            
-                dispatch(addItem(plant));
-                
-                //console.log('rendering plant id and name 2', plant.id, plant.name);
-            };
+        dispatch(addItem(plant));
+    };
 
            
-    
+    const calculateTotalQuantity= () => {
+        return cartItems ? cartItems.reduce((total, item) => total + item.quantity, 0): 0;
+    };
         
     
 
@@ -320,9 +319,15 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-                </div>
-            </div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                            <text className='cart_quantity'  x='130' y='155' fontSize='90' textAnchor='middle'>{calculateTotalQuantity()}</text> 
+                                        </svg>
+                                    </h1>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+            
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map(categoryObj => (
@@ -330,8 +335,7 @@ function ProductList({ onHomeClick }) {
                     <h2 className='product-list'>{categoryObj.category}</h2>
                     <div style={{ display: 'flex', gap: '20px' }}>
                     {categoryObj.plant && categoryObj.plant.map(plant => {
-                        //console.log('rendering plant id and name', plant.id, plant.name);
-                                           
+                                                                   
                                             return (
                                             <div key={plant.id} className="product-card">
                                                 <img className='product-image' src={plant.image} alt={plant.name} />
@@ -343,7 +347,7 @@ function ProductList({ onHomeClick }) {
                                                     onClick={() => handleAddToCart(plant)}
                                                     disabled={cartItems.some(item => item.id === plant.id)}
                                                 >
-                                                    {cartItems.some(item => item.id === plant.id) ? 'Added' : 'Add to Cart'}
+                                                    {cartItems.some(item => item.id === plant.id) ? 'Added to Cart' : 'Add to Cart'}
                                                 </button>
                                             </div>
                                             );
