d977b492682f33e8ab70ed62002b32413409fb21
vithursan6 <vithursanm@gmail.com>
2026-03-07 20:31:21 -0500

    Added coming soon alert functionality to checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,7 +21,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('Functionality Coming Soon!');
   };
 
   const handleIncrement = (item) => {
@@ -81,7 +81,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1"onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
